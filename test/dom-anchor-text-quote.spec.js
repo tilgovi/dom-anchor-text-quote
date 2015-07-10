f5495842b5994b33abe7e4d76031706a19059997
Randall Leeds <randall@bleeds.info>
2015-07-10 16:13:18 -0700

    Tests for the constructor

--- test/dom-anchor-text-quote.spec.js
+++ test/dom-anchor-text-quote.spec.js
@@ -17,6 +17,33 @@ describe('TextQuoteAnchor', () => {
     it('is a function', () => {
       assert.isFunction(TextQuoteAnchor);
     });
+
+    it('requires an exact argument', () => {
+      it('requires a range argument', () => {
+        let construct = () => new TextQuoteAnchor();
+        assert.throws(construct, 'required parameter');
+      });
+    });
+
+    it('stores the exact quote', () => {
+      let anchor = new TextQuoteAnchor('try heroic couplets');
+      assert.equal(anchor.exact, 'try heroic couplets');
+    });
+
+    it('accepts an optional context argument with prefix and suffix', () => {
+      let exact = 'They\'re, so to speak, semantic.';
+      let prefix = 'The urgencies are not policital or racial or social. ';
+      let suffix = ' Only the poetical enquiry can discovery what language ' +
+          'really is';
+      let context = {
+        prefix: prefix,
+        suffix: suffix,
+      };
+      let anchor = new TextQuoteAnchor(exact, context);
+      assert.equal(anchor.exact, exact);
+      assert.equal(anchor.prefix, prefix);
+      assert.equal(anchor.suffix, suffix);
+    });
   });
 
   describe('fromRange', () => {
