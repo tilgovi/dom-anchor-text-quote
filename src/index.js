029650c7d68f81d0a4278f5c0a9161948c6b1e46
Randall Leeds <randall@bleeds.info>
2016-09-25 19:58:39 -0700

    death to parentheses

--- src/index.js
+++ src/index.js
@@ -1,141 +1,141 @@
-import DiffMatchPatch from 'diff-match-patch';
-import * as textPosition from 'dom-anchor-text-position';
+import DiffMatchPatch from 'diff-match-patch'
+import * as textPosition from 'dom-anchor-text-position'
 
 // The DiffMatchPatch bitap has a hard 32-character pattern length limit.
-const SLICE_LENGTH = 32;
-const SLICE_RE = new RegExp('(.|[\r\n]){1,' + String(SLICE_LENGTH) + '}', 'g');
-const CONTEXT_LENGTH = SLICE_LENGTH;
+const SLICE_LENGTH = 32
+const SLICE_RE = new RegExp('(.|[\r\n]){1,' + String(SLICE_LENGTH) + '}', 'g')
+const CONTEXT_LENGTH = SLICE_LENGTH
 
 
 export default class TextQuoteAnchor {
   constructor(root, exact, context = {}) {
     if (root === undefined) {
-      throw new Error('missing required parameter "root"');
+      throw new Error('missing required parameter "root"')
     }
     if (exact === undefined) {
-      throw new Error('missing required parameter "exact"');
+      throw new Error('missing required parameter "exact"')
     }
-    this.root = root;
-    this.exact = exact;
-    this.prefix = context.prefix;
-    this.suffix = context.suffix;
+    this.root = root
+    this.exact = exact
+    this.prefix = context.prefix
+    this.suffix = context.suffix
   }
 
   static fromRange(root, range) {
     if (range === undefined) {
-      throw new Error('missing required parameter "range"');
+      throw new Error('missing required parameter "range"')
     }
 
-    let position = textPosition.fromRange(root, range);
-    position.root = root;
-    return this.fromPositionAnchor(position);
+    let position = textPosition.fromRange(root, range)
+    position.root = root
+    return this.fromPositionAnchor(position)
   }
 
   static fromSelector(root, selector = {}) {
-    return new TextQuoteAnchor(root, selector.exact, selector);
+    return new TextQuoteAnchor(root, selector.exact, selector)
   }
 
   static fromPositionAnchor(anchor) {
-    let root = anchor.root;
+    let root = anchor.root
 
-    let {start, end} = anchor;
-    let exact = root.textContent.substr(start, end - start);
+    let {start, end} = anchor
+    let exact = root.textContent.substr(start, end - start)
 
-    let prefixStart = Math.max(0, start - CONTEXT_LENGTH);
-    let prefix = root.textContent.substr(prefixStart, start - prefixStart);
+    let prefixStart = Math.max(0, start - CONTEXT_LENGTH)
+    let prefix = root.textContent.substr(prefixStart, start - prefixStart)
 
-    let suffixEnd = Math.min(root.textContent.length, end + CONTEXT_LENGTH);
-    let suffix = root.textContent.substr(end, suffixEnd - end);
+    let suffixEnd = Math.min(root.textContent.length, end + CONTEXT_LENGTH)
+    let suffix = root.textContent.substr(end, suffixEnd - end)
 
-    return new TextQuoteAnchor(root, exact, {prefix, suffix});
+    return new TextQuoteAnchor(root, exact, {prefix, suffix})
   }
 
   toRange(options) {
-    let position = this.toPositionAnchor(options);
-    return textPosition.toRange(this.root, position);
+    let position = this.toPositionAnchor(options)
+    return textPosition.toRange(this.root, position)
   }
 
   toSelector() {
     let selector = {
       type: 'TextQuoteSelector',
       exact: this.exact,
-    };
-    if (this.prefix !== undefined) selector.prefix = this.prefix;
-    if (this.suffix !== undefined) selector.suffix = this.suffix;
-    return selector;
+    }
+    if (this.prefix !== undefined) selector.prefix = this.prefix
+    if (this.suffix !== undefined) selector.suffix = this.suffix
+    return selector
   }
 
   toPositionAnchor(options = {}) {
-    let {hint} = options;
-    let root = this.root;
-    let dmp = new DiffMatchPatch();
+    let {hint} = options
+    let root = this.root
+    let dmp = new DiffMatchPatch()
 
-    dmp.Match_Distance = root.textContent.length * 2;
+    dmp.Match_Distance = root.textContent.length * 2
 
     // Work around a hard limit of the DiffMatchPatch bitap implementation.
     // The search pattern must be no more than SLICE_LENGTH characters.
-    let slices = this.exact.match(SLICE_RE);
-    let loc = (hint === undefined) ? ((root.textContent.length / 2) | 0) : hint;
-    let start = Number.POSITIVE_INFINITY;
-    let end = Number.NEGATIVE_INFINITY;
-    let result = -1;
-    let havePrefix = this.prefix !== undefined;
-    let haveSuffix = this.suffix !== undefined;
-    let foundPrefix = false;
+    let slices = this.exact.match(SLICE_RE)
+    let loc = (hint === undefined) ? ((root.textContent.length / 2) | 0) : hint
+    let start = Number.POSITIVE_INFINITY
+    let end = Number.NEGATIVE_INFINITY
+    let result = -1
+    let havePrefix = this.prefix !== undefined
+    let haveSuffix = this.suffix !== undefined
+    let foundPrefix = false
 
     // If the prefix is known then search for that first.
     if (havePrefix) {
-      result = dmp.match_main(root.textContent, this.prefix, loc);
+      result = dmp.match_main(root.textContent, this.prefix, loc)
       if (result > -1) {
-        loc = result + this.prefix.length;
-        foundPrefix = true;
+        loc = result + this.prefix.length
+        foundPrefix = true
       }
     }
 
     // If we have a suffix, and the prefix wasn't found, then search for it.
     if (haveSuffix && !foundPrefix) {
-      result = dmp.match_main(root.textContent, this.suffix, loc + this.exact.length);
+      result = dmp.match_main(root.textContent, this.suffix, loc + this.exact.length)
       if (result > -1) {
-        loc = result - this.exact.length;
+        loc = result - this.exact.length
       }
     }
 
     // Search for the first slice.
-    let firstSlice = slices.shift();
-    result = dmp.match_main(root.textContent, firstSlice, loc);
+    let firstSlice = slices.shift()
+    result = dmp.match_main(root.textContent, firstSlice, loc)
     if (result > -1) {
-      start = result;
-      loc = end = start + firstSlice.length;
+      start = result
+      loc = end = start + firstSlice.length
     } else {
-      throw new Error('no match found');
+      throw new Error('no match found')
     }
 
     // Create a fold function that will reduce slices to positional extents.
     let foldSlices = (acc, slice) => {
-      let result = dmp.match_main(root.textContent, slice, acc.loc);
+      let result = dmp.match_main(root.textContent, slice, acc.loc)
       if (result === -1) {
-        throw new Error('no match found');
+        throw new Error('no match found')
       }
 
       // The next slice should follow this one closely.
-      acc.loc = result + slice.length;
+      acc.loc = result + slice.length
 
       // Expand the start and end to a quote that includes all the slices.
-      acc.start = Math.min(acc.start, result);
-      acc.end = Math.max(acc.end, result + slice.length);
+      acc.start = Math.min(acc.start, result)
+      acc.end = Math.max(acc.end, result + slice.length)
 
-      return acc;
-    };
+      return acc
+    }
 
     // Use the fold function to establish the full quote extents.
     // Expect the slices to be close to one another.
     // This distance is deliberately generous for now.
-    dmp.Match_Distance = 64;
+    dmp.Match_Distance = 64
     let acc = slices.reduce(foldSlices, {
       start: start,
       end: end,
       loc: loc,
-    });
+    })
 
     return {start: acc.start, end: acc.end}
   }
--- test/index_spec.js
+++ test/index_spec.js
@@ -1,238 +1,238 @@
-import TextQuoteAnchor from '../src';
+import TextQuoteAnchor from '../src'
 
 describe('TextQuoteAnchor', () => {
   before(() => {
-    fixture.setBase('test/fixtures');
-  });
+    fixture.setBase('test/fixtures')
+  })
 
   beforeEach(() => {
-    fixture.load('test.html');
-  });
+    fixture.load('test.html')
+  })
 
   afterEach(() => {
-    fixture.cleanup();
-  });
+    fixture.cleanup()
+  })
 
   describe('constructor', () => {
     it('is a function', () => {
-      assert.isFunction(TextQuoteAnchor);
-    });
+      assert.isFunction(TextQuoteAnchor)
+    })
 
     it('requires a root argument', () => {
-      let construct = () => new TextQuoteAnchor();
-      assert.throws(construct, 'required parameter');
-    });
+      let construct = () => new TextQuoteAnchor()
+      assert.throws(construct, 'required parameter')
+    })
 
     it('requires an exact argument', () => {
-      let construct = () => new TextQuoteAnchor(fixture.el);
-      assert.throws(construct, 'required parameter');
-    });
+      let construct = () => new TextQuoteAnchor(fixture.el)
+      assert.throws(construct, 'required parameter')
+    })
 
     it('stores the exact quote', () => {
-      let anchor = new TextQuoteAnchor(fixture.el, 'try heroic couplets');
-      assert.equal(anchor.exact, 'try heroic couplets');
-    });
+      let anchor = new TextQuoteAnchor(fixture.el, 'try heroic couplets')
+      assert.equal(anchor.exact, 'try heroic couplets')
+    })
 
     it('accepts an optional context argument with prefix and suffix', () => {
-      let exact = 'They\'re, so to speak, semantic.';
-      let prefix = 'The urgencies are not policital or racial or social. ';
+      let exact = 'They\'re, so to speak, semantic.'
+      let prefix = 'The urgencies are not policital or racial or social. '
       let suffix = ' Only the poetical enquiry can discovery what language ' +
-          'really is';
+          'really is'
       let context = {
         prefix: prefix,
         suffix: suffix,
-      };
-      let anchor = new TextQuoteAnchor(fixture.el, exact, context);
-      assert.instanceOf(anchor, TextQuoteAnchor);
-      assert.equal(anchor.root, fixture.el);
-      assert.equal(anchor.exact, exact);
-      assert.equal(anchor.prefix, prefix);
-      assert.equal(anchor.suffix, suffix);
-    });
-  });
+      }
+      let anchor = new TextQuoteAnchor(fixture.el, exact, context)
+      assert.instanceOf(anchor, TextQuoteAnchor)
+      assert.equal(anchor.root, fixture.el)
+      assert.equal(anchor.exact, exact)
+      assert.equal(anchor.prefix, prefix)
+      assert.equal(anchor.suffix, suffix)
+    })
+  })
 
   describe('fromRange', () => {
     it('requires a root argument', () => {
-      let construct = () => TextQuoteAnchor.fromRange();
-      assert.throws(construct, 'required parameter');
-    });
+      let construct = () => TextQuoteAnchor.fromRange()
+      assert.throws(construct, 'required parameter')
+    })
 
     it('requires a range argument', () => {
-      let construct = () => TextQuoteAnchor.fromRange(fixture.el);
-      assert.throws(construct, 'required parameter');
-    });
+      let construct = () => TextQuoteAnchor.fromRange(fixture.el)
+      assert.throws(construct, 'required parameter')
+    })
 
     it('returns a TextQuoteAnchor with context', () => {
-      let root = fixture.el;
-      let range = document.createRange();
-      let node = root.querySelector('code');
-      range.selectNodeContents(node);
-      let anchor = TextQuoteAnchor.fromRange(root, range);
-      assert.instanceOf(anchor, TextQuoteAnchor);
-      assert.equal(anchor.exact, 'commodo vitae');
-    });
-  });
+      let root = fixture.el
+      let range = document.createRange()
+      let node = root.querySelector('code')
+      range.selectNodeContents(node)
+      let anchor = TextQuoteAnchor.fromRange(root, range)
+      assert.instanceOf(anchor, TextQuoteAnchor)
+      assert.equal(anchor.exact, 'commodo vitae')
+    })
+  })
 
   describe('fromSelector', () => {
     it('requires a root argument', () => {
-      let construct = () => TextQuoteAnchor.fromSelector();
-      assert.throws(construct, 'required parameter');
-    });
+      let construct = () => TextQuoteAnchor.fromSelector()
+      assert.throws(construct, 'required parameter')
+    })
 
     it('requires a selector argument', () => {
-      let construct = () => TextQuoteAnchor.fromSelector(fixture.el);
-      assert.throws(construct, 'required parameter');
-    });
+      let construct = () => TextQuoteAnchor.fromSelector(fixture.el)
+      assert.throws(construct, 'required parameter')
+    })
 
     it('returns a TextQuoteAnchor from a selector with exact quote', () => {
       let selector = {
         exact: 'You can\'t split life into diachronic segments.',
-      };
-      let anchor = TextQuoteAnchor.fromSelector(fixture.el, selector);
-      assert.instanceOf(anchor, TextQuoteAnchor);
-      assert.equal(anchor.root, fixture.el);
-      assert.equal(anchor.exact, selector.exact);
-    });
+      }
+      let anchor = TextQuoteAnchor.fromSelector(fixture.el, selector)
+      assert.instanceOf(anchor, TextQuoteAnchor)
+      assert.equal(anchor.root, fixture.el)
+      assert.equal(anchor.exact, selector.exact)
+    })
 
     it('returns a TextQuoteAnchor from a selector with context', () => {
       let selector = {
         exact: 'Friday the twenty-sixth.',
         prefix: 'Today was what? ',
         suffix: 'There would be a salary cheque for him on Monday.',
-      };
-      let anchor = TextQuoteAnchor.fromSelector(fixture.el, selector);
-      assert.instanceOf(anchor, TextQuoteAnchor);
-      assert.equal(anchor.root, fixture.el);
-      assert.equal(anchor.exact, selector.exact);
-      assert.equal(anchor.prefix, selector.prefix);
-      assert.equal(anchor.suffix, selector.suffix);
-    });
-  });
+      }
+      let anchor = TextQuoteAnchor.fromSelector(fixture.el, selector)
+      assert.instanceOf(anchor, TextQuoteAnchor)
+      assert.equal(anchor.root, fixture.el)
+      assert.equal(anchor.exact, selector.exact)
+      assert.equal(anchor.prefix, selector.prefix)
+      assert.equal(anchor.suffix, selector.suffix)
+    })
+  })
 
   describe('toRange', () => {
     it('finds an exact quote', () => {
-      let anchor = new TextQuoteAnchor(fixture.el, 'commodo vitae');
-      let range = anchor.toRange();
-      let text = range.toString();
-      assert.equal(text, 'commodo vitae');
-    });
+      let anchor = new TextQuoteAnchor(fixture.el, 'commodo vitae')
+      let range = anchor.toRange()
+      let text = range.toString()
+      assert.equal(text, 'commodo vitae')
+    })
 
     it('finds an exact quote longer than 32 characters', () => {
-      let expected = 'Quisque sit amet est et sapien ullamcorper pharetra';
-      let anchor = new TextQuoteAnchor(fixture.el, expected);
-      let range = anchor.toRange();
-      let text = range.toString();
-      assert.equal(text, expected);
-    });
+      let expected = 'Quisque sit amet est et sapien ullamcorper pharetra'
+      let anchor = new TextQuoteAnchor(fixture.el, expected)
+      let range = anchor.toRange()
+      let text = range.toString()
+      assert.equal(text, expected)
+    })
 
     it('finds a close exact quote', () => {
-      let anchor = new TextQuoteAnchor(fixture.el, 'commodo cites');
-      let range = anchor.toRange();
-      let text = range.toString();
-      assert.equal(text, 'commodo vitae');
-    });
+      let anchor = new TextQuoteAnchor(fixture.el, 'commodo cites')
+      let range = anchor.toRange()
+      let text = range.toString()
+      assert.equal(text, 'commodo vitae')
+    })
 
     it('finds a context quote', () => {
-      let exact = 'commodo vitae';
-      let prefix = 'condimentum sed, ';
-      let suffix = ', ornare sit amet';
-      let context = {prefix, suffix};
-      let anchor = new TextQuoteAnchor(fixture.el, exact, context);
-      let range = anchor.toRange();
-      let text = range.toString();
-      assert.equal(text, 'commodo vitae');
-    });
+      let exact = 'commodo vitae'
+      let prefix = 'condimentum sed, '
+      let suffix = ', ornare sit amet'
+      let context = {prefix, suffix}
+      let anchor = new TextQuoteAnchor(fixture.el, exact, context)
+      let range = anchor.toRange()
+      let text = range.toString()
+      assert.equal(text, 'commodo vitae')
+    })
 
     it('finds a close context quote', () => {
-      let exact = 'commodo cites';
-      let prefix = 'condimentum sed, ';
-      let suffix = ', ornare sit amet';
-      let context = {prefix, suffix};
-      let anchor = new TextQuoteAnchor(fixture.el, exact, context);
-      let range = anchor.toRange();
-      let text = range.toString();
-      assert.equal(text, 'commodo vitae');
-    });
+      let exact = 'commodo cites'
+      let prefix = 'condimentum sed, '
+      let suffix = ', ornare sit amet'
+      let context = {prefix, suffix}
+      let anchor = new TextQuoteAnchor(fixture.el, exact, context)
+      let range = anchor.toRange()
+      let text = range.toString()
+      assert.equal(text, 'commodo vitae')
+    })
 
     it('can disambiguate using the prefix', () => {
-      let exact = 'on';
+      let exact = 'on'
       let context = {
         prefix: 'Donec n',
-      };
-      let anchor = new TextQuoteAnchor(fixture.el, exact, context);
-      let range = anchor.toRange();
-      let text = range.toString();
-      let textNode = range.commonAncestorContainer;
-      let anchorNode = textNode.parentNode;
-      assert.equal(text, 'on');
-      assert.equal(anchorNode.tagName, 'A');
-    });
+      }
+      let anchor = new TextQuoteAnchor(fixture.el, exact, context)
+      let range = anchor.toRange()
+      let text = range.toString()
+      let textNode = range.commonAncestorContainer
+      let anchorNode = textNode.parentNode
+      assert.equal(text, 'on')
+      assert.equal(anchorNode.tagName, 'A')
+    })
 
     it('can disambiguate using the suffix', () => {
-      let exact = 'on';
+      let exact = 'on'
       let context = {
         suffix: ' enim in',
-      };
-      let anchor = new TextQuoteAnchor(fixture.el, exact, context);
-      let range = anchor.toRange();
-      let text = range.toString();
-      let textNode = range.commonAncestorContainer;
-      let anchorNode = textNode.parentNode;
-      assert.equal(text, 'on');
-      assert.equal(anchorNode.tagName, 'A');
-    });
+      }
+      let anchor = new TextQuoteAnchor(fixture.el, exact, context)
+      let range = anchor.toRange()
+      let text = range.toString()
+      let textNode = range.commonAncestorContainer
+      let anchorNode = textNode.parentNode
+      assert.equal(text, 'on')
+      assert.equal(anchorNode.tagName, 'A')
+    })
 
     it('succeeds with the best match even if the context fails', () => {
-      let exact = 'commodo vitae';
+      let exact = 'commodo vitae'
       let context = {
         prefix: 'bogomips',
         suffix: 'bogomips',
-      };
-      let anchor = new TextQuoteAnchor(fixture.el, exact, context);
-      let range = anchor.toRange();
-    });
+      }
+      let anchor = new TextQuoteAnchor(fixture.el, exact, context)
+      let range = anchor.toRange()
+    })
 
     it('throws an error when the quote is not found', () => {
-      let exact = 'bogus';
-      let anchor = new TextQuoteAnchor(fixture.el, exact);
-      let attempt = () => anchor.toRange();
-      assert.throws(attempt, 'no match found');
-    });
+      let exact = 'bogus'
+      let anchor = new TextQuoteAnchor(fixture.el, exact)
+      let attempt = () => anchor.toRange()
+      assert.throws(attempt, 'no match found')
+    })
 
     it('throws an error when a long quote is not found', () => {
-      let expected = 'Quisque sit amet est et sapien ullam triceracorn';
-      let anchor = new TextQuoteAnchor(fixture.el, expected);
-      let attempt = () => anchor.toRange();
-      assert.throws(attempt, 'no match found');
-    });
+      let expected = 'Quisque sit amet est et sapien ullam triceracorn'
+      let anchor = new TextQuoteAnchor(fixture.el, expected)
+      let attempt = () => anchor.toRange()
+      assert.throws(attempt, 'no match found')
+    })
 
     it('uses a hint option to prioritize matches', () => {
-      let expected = 'Aenean';
-      let anchor = new TextQuoteAnchor(fixture.el, expected);
-      let first = fixture.el.textContent.indexOf('Aenean');
-      let last = fixture.el.textContent.lastIndexOf('Aenean');
-      let rangeFirst = anchor.toRange({hint: first});
-      let rangeLast = anchor.toRange({hint: last});
-      assert.notEqual(rangeFirst.startContainer, rangeLast.startContainer);
-    });
-  });
+      let expected = 'Aenean'
+      let anchor = new TextQuoteAnchor(fixture.el, expected)
+      let first = fixture.el.textContent.indexOf('Aenean')
+      let last = fixture.el.textContent.lastIndexOf('Aenean')
+      let rangeFirst = anchor.toRange({hint: first})
+      let rangeLast = anchor.toRange({hint: last})
+      assert.notEqual(rangeFirst.startContainer, rangeLast.startContainer)
+    })
+  })
 
   describe('toSelector', () => {
     it('returns a selector for the stored exact quote', () => {
-      let anchor = new TextQuoteAnchor(fixture.el, 'a');
-      let selector = anchor.toSelector();
-      assert.equal(selector.type, 'TextQuoteSelector');
-      assert.equal(selector.exact, 'a');
-    });
+      let anchor = new TextQuoteAnchor(fixture.el, 'a')
+      let selector = anchor.toSelector()
+      assert.equal(selector.type, 'TextQuoteSelector')
+      assert.equal(selector.exact, 'a')
+    })
 
     it('returns a selector for the stored context quote', () => {
-      let context = {prefix: 'b', suffix: 'c'};
-      let anchor = new TextQuoteAnchor(fixture.el, 'a', context);
-      let selector = anchor.toSelector();
-      assert.equal(selector.type, 'TextQuoteSelector');
-      assert.equal(selector.exact, 'a');
-      assert.equal(selector.prefix, 'b');
-      assert.equal(selector.suffix, 'c');
-    });
-  });
-});
+      let context = {prefix: 'b', suffix: 'c'}
+      let anchor = new TextQuoteAnchor(fixture.el, 'a', context)
+      let selector = anchor.toSelector()
+      assert.equal(selector.type, 'TextQuoteSelector')
+      assert.equal(selector.exact, 'a')
+      assert.equal(selector.prefix, 'b')
+      assert.equal(selector.suffix, 'c')
+    })
+  })
+})
