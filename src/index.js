823ab5f1712d628b2ab462712bcf3891209090fc
Randall Leeds <randall@bleeds.info>
2016-09-25 22:07:40 -0700

    begin de-OOP

--- src/index.js
+++ src/index.js
@@ -7,6 +7,150 @@ const SLICE_RE = new RegExp('(.|[\r\n]){1,' + String(SLICE_LENGTH) + '}', 'g')
 const CONTEXT_LENGTH = SLICE_LENGTH
 
 
+function fromRange(root, range) {
+  if (root === undefined) {
+    throw new Error('missing required parameter "root"')
+  }
+  if (range === undefined) {
+    throw new Error('missing required parameter "range"')
+  }
+
+  let positionSelector = textPosition.fromRange(root, range)
+
+  return fromTextPosition(root, positionSelector)
+}
+
+
+function fromTextPosition(root, selector) {
+  if (root === undefined) {
+    throw new Error('missing required parameter "root"')
+  }
+  if (selector === undefined) {
+    throw new Error('missing required parameter "selector"')
+  }
+
+  let {start} = selector
+  if (start === undefined) {
+    throw new Error('selector missing required property "start"')
+  }
+  if (start < 0) {
+    throw new Error('property "start" must be a non-negative integer')
+  }
+
+  let {end} = selector
+  if (end === undefined) {
+    throw new Error('selector missing required property "end"')
+  }
+  if (end < 0) {
+    throw new Error('property "end" must be a non-negative integer')
+  }
+
+  let exact = root.textContent.substr(start, end - start)
+
+  let prefixStart = Math.max(0, start - CONTEXT_LENGTH)
+  let prefix = root.textContent.substr(prefixStart, start - prefixStart)
+
+  let suffixEnd = Math.min(root.textContent.length, end + CONTEXT_LENGTH)
+  let suffix = root.textContent.substr(end, suffixEnd - end)
+
+  return {exact, prefix, suffix}
+}
+
+
+function toRange(root, selector, options = {}) {
+  let position = toTextPosition(root, selector, options)
+  return textPosition.toRange(root, position)
+}
+
+
+function toTextPosition(root, selector, options = {}) {
+  if (root === undefined) {
+    throw new Error('missing required parameter "root"')
+  }
+  if (selector === undefined) {
+    throw new Error('missing required parameter "selector"')
+  }
+
+  let {exact} = selector
+  if (exact === undefined) {
+    throw new Error('selector missing required property "exact"')
+  }
+
+  let {prefix, suffix} = selector
+  let {hint} = options
+  let dmp = new DiffMatchPatch()
+
+  dmp.Match_Distance = root.textContent.length * 2
+
+  // Work around a hard limit of the DiffMatchPatch bitap implementation.
+  // The search pattern must be no more than SLICE_LENGTH characters.
+  let slices = exact.match(SLICE_RE)
+  let loc = (hint === undefined) ? ((root.textContent.length / 2) | 0) : hint
+  let start = Number.POSITIVE_INFINITY
+  let end = Number.NEGATIVE_INFINITY
+  let result = -1
+  let havePrefix = prefix !== undefined
+  let haveSuffix = suffix !== undefined
+  let foundPrefix = false
+
+  // If the prefix is known then search for that first.
+  if (havePrefix) {
+    result = dmp.match_main(root.textContent, prefix, loc)
+    if (result > -1) {
+      loc = result + prefix.length
+      foundPrefix = true
+    }
+  }
+
+  // If we have a suffix, and the prefix wasn't found, then search for it.
+  if (haveSuffix && !foundPrefix) {
+    result = dmp.match_main(root.textContent, suffix, loc + exact.length)
+    if (result > -1) {
+      loc = result - exact.length
+    }
+  }
+
+  // Search for the first slice.
+  let firstSlice = slices.shift()
+  result = dmp.match_main(root.textContent, firstSlice, loc)
+  if (result > -1) {
+    start = result
+    loc = end = start + firstSlice.length
+  } else {
+    throw new Error('no match found')
+  }
+
+  // Create a fold function that will reduce slices to positional extents.
+  let foldSlices = (acc, slice) => {
+    let result = dmp.match_main(root.textContent, slice, acc.loc)
+    if (result === -1) {
+      throw new Error('no match found')
+    }
+
+    // The next slice should follow this one closely.
+    acc.loc = result + slice.length
+
+    // Expand the start and end to a quote that includes all the slices.
+    acc.start = Math.min(acc.start, result)
+    acc.end = Math.max(acc.end, result + slice.length)
+
+    return acc
+  }
+
+  // Use the fold function to establish the full quote extents.
+  // Expect the slices to be close to one another.
+  // This distance is deliberately generous for now.
+  dmp.Match_Distance = 64
+  let acc = slices.reduce(foldSlices, {
+    start: start,
+    end: end,
+    loc: loc,
+  })
+
+  return {start: acc.start, end: acc.end}
+}
+
+
 export default class TextQuoteAnchor {
   constructor(root, exact, context = {}) {
     if (root === undefined) {
@@ -22,13 +166,8 @@ export default class TextQuoteAnchor {
   }
 
   static fromRange(root, range) {
-    if (range === undefined) {
-      throw new Error('missing required parameter "range"')
-    }
-
-    let position = textPosition.fromRange(root, range)
-    position.root = root
-    return this.fromPositionAnchor(position)
+    let selector = fromRange(root, range)
+    return new TextQuoteAnchor(root, selector.exact, selector)
   }
 
   static fromSelector(root, selector = {}) {
@@ -36,23 +175,12 @@ export default class TextQuoteAnchor {
   }
 
   static fromPositionAnchor(anchor) {
-    let root = anchor.root
-
-    let {start, end} = anchor
-    let exact = root.textContent.substr(start, end - start)
-
-    let prefixStart = Math.max(0, start - CONTEXT_LENGTH)
-    let prefix = root.textContent.substr(prefixStart, start - prefixStart)
-
-    let suffixEnd = Math.min(root.textContent.length, end + CONTEXT_LENGTH)
-    let suffix = root.textContent.substr(end, suffixEnd - end)
-
-    return new TextQuoteAnchor(root, exact, {prefix, suffix})
+    let quote = fromTextPosition(anchor.root, anchor)
+    return new TextQuoteAnchor(anchor.root, quote.exact, quote)
   }
 
   toRange(options) {
-    let position = this.toPositionAnchor(options)
-    return textPosition.toRange(this.root, position)
+    return toRange(this.root, this, options)
   }
 
   toSelector() {
@@ -66,77 +194,6 @@ export default class TextQuoteAnchor {
   }
 
   toPositionAnchor(options = {}) {
-    let {hint} = options
-    let root = this.root
-    let dmp = new DiffMatchPatch()
-
-    dmp.Match_Distance = root.textContent.length * 2
-
-    // Work around a hard limit of the DiffMatchPatch bitap implementation.
-    // The search pattern must be no more than SLICE_LENGTH characters.
-    let slices = this.exact.match(SLICE_RE)
-    let loc = (hint === undefined) ? ((root.textContent.length / 2) | 0) : hint
-    let start = Number.POSITIVE_INFINITY
-    let end = Number.NEGATIVE_INFINITY
-    let result = -1
-    let havePrefix = this.prefix !== undefined
-    let haveSuffix = this.suffix !== undefined
-    let foundPrefix = false
-
-    // If the prefix is known then search for that first.
-    if (havePrefix) {
-      result = dmp.match_main(root.textContent, this.prefix, loc)
-      if (result > -1) {
-        loc = result + this.prefix.length
-        foundPrefix = true
-      }
-    }
-
-    // If we have a suffix, and the prefix wasn't found, then search for it.
-    if (haveSuffix && !foundPrefix) {
-      result = dmp.match_main(root.textContent, this.suffix, loc + this.exact.length)
-      if (result > -1) {
-        loc = result - this.exact.length
-      }
-    }
-
-    // Search for the first slice.
-    let firstSlice = slices.shift()
-    result = dmp.match_main(root.textContent, firstSlice, loc)
-    if (result > -1) {
-      start = result
-      loc = end = start + firstSlice.length
-    } else {
-      throw new Error('no match found')
-    }
-
-    // Create a fold function that will reduce slices to positional extents.
-    let foldSlices = (acc, slice) => {
-      let result = dmp.match_main(root.textContent, slice, acc.loc)
-      if (result === -1) {
-        throw new Error('no match found')
-      }
-
-      // The next slice should follow this one closely.
-      acc.loc = result + slice.length
-
-      // Expand the start and end to a quote that includes all the slices.
-      acc.start = Math.min(acc.start, result)
-      acc.end = Math.max(acc.end, result + slice.length)
-
-      return acc
-    }
-
-    // Use the fold function to establish the full quote extents.
-    // Expect the slices to be close to one another.
-    // This distance is deliberately generous for now.
-    dmp.Match_Distance = 64
-    let acc = slices.reduce(foldSlices, {
-      start: start,
-      end: end,
-      loc: loc,
-    })
-
-    return {start: acc.start, end: acc.end}
+    return toTextPosition(this.root, this, options)
   }
 }
