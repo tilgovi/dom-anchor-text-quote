1f8b133112013ef31c084d6f6eaf623e1a0ce56a
Robert Knight <robertknight@gmail.com>
2017-02-10 10:53:14 +0000

    Refactor check for `acc` being null
    As suggested in https://github.com/tilgovi/dom-anchor-text-quote/pull/11#pullrequestreview-19681671

--- src/index.js
+++ src/index.js
@@ -125,6 +125,11 @@ export function toTextPosition(root, selector, options = {}) {
 
   // Create a fold function that will reduce slices to positional extents.
   let foldSlices = (acc, slice) => {
+    if (!acc) {
+      // A search for an earlier slice of the pattern failed to match.
+      return null
+    }
+
     let result = dmp.match_main(root.textContent, slice, acc.loc)
     if (result === -1) {
       return null
@@ -144,13 +149,7 @@ export function toTextPosition(root, selector, options = {}) {
   // Expect the slices to be close to one another.
   // This distance is deliberately generous for now.
   dmp.Match_Distance = 64
-  const acc = slices.reduce((acc, slice) => {
-    if (!acc) {
-      return null
-    }
-    return foldSlices(acc, slice)
-  }, {start, end, loc});
-
+  const acc = slices.reduce(foldSlices, {start, end, loc})
   if (!acc) {
     return null
   }
